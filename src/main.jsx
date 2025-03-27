840dad31b8057b0674267277d38a1775ec58e0a3
Sachin <sachin@appentus.in>
2025-03-27 23:36:35 +0530

    update for hash routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import store from "./store";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter >
-    <ToastContainer position="top-right"
-    autoClose={2000}
-    ProgressBar={true}
-    newestOnTop={false}
-    closeOnClick={false}
-    rtl={false}
-    pauseOnHover={false}/>
-    <App />
-    </BrowserRouter>
+      <HashRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          ProgressBar={true}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnHover={false}
+        />
+        <App />
+      </HashRouter>
     </Provider>
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
