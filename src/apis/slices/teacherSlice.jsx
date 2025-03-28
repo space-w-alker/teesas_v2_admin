92275310147dbaf8f660fc409a174f5e967d5f6e
NitinAppentus <106055849+NitinAppentus@users.noreply.github.com>
2025-03-28 17:58:21 +0530

    Update teacherSlice.jsx

--- src/apis/slices/teacherSlice.jsx
+++ src/apis/slices/teacherSlice.jsx
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getAPICall, postAPICall,postFileAPICall,deleteAPICall } from "../client/methodCalls";
+import { getAPICall, postAPICall, postFileAPICall, deleteAPICall } from "../client/methodCalls";
 import { toast } from "react-toastify";
 
 import { config } from "../client/config";
 
-const {GET_TEACHERS, GET_TEACHER_STATISTICS, UPDATE_TEACHER,BASEURL,ADD_TEACHER,GET_TEACHER_DETAILS,GET_LOCAL_GOV,GET_LIVE_CLASS_TEACHERS  } = config;
+const { GET_TEACHERS, GET_TEACHER_STATISTICS, UPDATE_TEACHER, BASEURL, ADD_TEACHER, GET_TEACHER_DETAILS, GET_LOCAL_GOV, GET_LIVE_CLASS_TEACHERS } = config;
 
 export const teacherSlice = createSlice({
   name: "teacher",
@@ -15,23 +15,23 @@ export const teacherSlice = createSlice({
     getTeacherStatsResponse: {
       response: {},
     },
-    updateTeacherResponse: { 
-      response:{},
+    updateTeacherResponse: {
+      response: {},
     },
     addTeacherResponse: {
       response: {},
     },
-    getTeacherDetailsResponse:{
-        response: {},
+    getTeacherDetailsResponse: {
+      response: {},
     },
-    getLocalGovResponse:{
-        response: {},
+    getLocalGovResponse: {
+      response: {},
     },
     getLiveClassTeachersResponse: {
       response: {},
     },
-    deleteTeacherResponse: { 
-      response:{},
+    deleteTeacherResponse: {
+      response: {},
     },
   },
   reducers: {
@@ -47,13 +47,13 @@ export const teacherSlice = createSlice({
     addTeacher: (state, action) => {
       state.addTeacherResponse = action.payload;
     },
-    getTeacherDetails:(state,action) =>{
-       state.getTeacherDetailsResponse = action.payload
+    getTeacherDetails: (state, action) => {
+      state.getTeacherDetailsResponse = action.payload
     },
-    getLocalGov:(state,action) =>{
-        state.getLocalGovResponse = action.payload
-     },
-     getLiveClassTeachers: (state, action) => {
+    getLocalGov: (state, action) => {
+      state.getLocalGovResponse = action.payload
+    },
+    getLiveClassTeachers: (state, action) => {
       state.getLiveClassTeachersResponse = action.payload;
     },
     deleteTeacher: (state, action) => {
@@ -97,7 +97,7 @@ export const deleteTeacherAsync = async ({
         callbackFn && callbackFn(data);
         dispatch(deleteTeacher({ isLoading: false, response: result.data }));
       } else {
-        toast.error(res?.data?.message);
+        toast.error("Can not Delete Teacher Live class is Created");
       }
     });
   } catch (err) {
@@ -118,11 +118,11 @@ export const getTeacherStatsAsync = async ({ dispatch, callbackFn, data, token }
   }
 };
 
-export const updateTeacherAsync = async ({ dispatch, body, callbackFn,token }) => {
+export const updateTeacherAsync = async ({ dispatch, body, callbackFn, token }) => {
   try {
     // dispatch(UserLogin({ isLoading: true }));
     const URL = `${BASEURL}${UPDATE_TEACHER}`;
-    const result = await postAPICall(URL, body,true,token).then((res) => {
+    const result = await postAPICall(URL, body, true, token).then((res) => {
       callbackFn && callbackFn(res);
       return res;
     });
@@ -133,66 +133,66 @@ export const updateTeacherAsync = async ({ dispatch, body, callbackFn,token }) =
 };
 
 export const getTeacherDetailsAsync = async ({ dispatch, callbackFn, data, token }) => {
-    try {
-      const URL = `${BASEURL}${GET_TEACHER_DETAILS}`;
-      const result = getAPICall(URL, data, token).then((res) => {
-        callbackFn && callbackFn(res);
-        return res;
-      });
-      dispatch(getTeacherDetails({ isLoading: false, response: result.data }));
-    } catch (err) {
-      dispatch(getTeacherDetails({ isLoading: false }));
-    }
-  };
+  try {
+    const URL = `${BASEURL}${GET_TEACHER_DETAILS}`;
+    const result = getAPICall(URL, data, token).then((res) => {
+      callbackFn && callbackFn(res);
+      return res;
+    });
+    dispatch(getTeacherDetails({ isLoading: false, response: result.data }));
+  } catch (err) {
+    dispatch(getTeacherDetails({ isLoading: false }));
+  }
+};
 
 
-export const addTeacherAsync = async ({ dispatch, body, callbackFn,token }) => {
-    try {
-      // dispatch(UserLogin({ isLoading: true }));
-      const URL = `${BASEURL}${ADD_TEACHER}`;
-      const result = await postAPICall(URL, body,token).then((res) => {
-        callbackFn && callbackFn(res);
-        return res;
-      });
-      dispatch(addTeacher({ isLoading: false, response: result.data }));
-    } catch (error) {
-      dispatch(addTeacher({ isLoading: false }));
-    }
-  };
+export const addTeacherAsync = async ({ dispatch, body, callbackFn, token }) => {
+  try {
+    // dispatch(UserLogin({ isLoading: true }));
+    const URL = `${BASEURL}${ADD_TEACHER}`;
+    const result = await postAPICall(URL, body, token).then((res) => {
+      callbackFn && callbackFn(res);
+      return res;
+    });
+    dispatch(addTeacher({ isLoading: false, response: result.data }));
+  } catch (error) {
+    dispatch(addTeacher({ isLoading: false }));
+  }
+};
 
-  export const getLocalGovAsync = async ({ dispatch, callbackFn, data, token }) => {
-    try {
-      const URL = `${BASEURL}${GET_LOCAL_GOV}`;
-      const result = getAPICall(URL, data, token).then((res) => {
-        callbackFn && callbackFn(res);
-        return res;
-      });
-      dispatch(getLocalGov({ isLoading: false, response: result.data }));
-    } catch (err) {
-      dispatch(getLocalGov({ isLoading: false }));
-    }
-  };
+export const getLocalGovAsync = async ({ dispatch, callbackFn, data, token }) => {
+  try {
+    const URL = `${BASEURL}${GET_LOCAL_GOV}`;
+    const result = getAPICall(URL, data, token).then((res) => {
+      callbackFn && callbackFn(res);
+      return res;
+    });
+    dispatch(getLocalGov({ isLoading: false, response: result.data }));
+  } catch (err) {
+    dispatch(getLocalGov({ isLoading: false }));
+  }
+};
 
 
-  export const getLiveClassTeachersAsync = async ({ dispatch, callbackFn, data, token }) => {
-    try {
-      const URL = `${BASEURL}${GET_LIVE_CLASS_TEACHERS}`;
-      const result = getAPICall(URL, data, token).then((res) => {
-        callbackFn && callbackFn(res);
-        return res;
-      });
-      dispatch(getLiveClassTeachers({ isLoading: false, response: result.data }));
-    } catch (err) {
-      dispatch(getLiveClassTeachers({ isLoading: false }));
-    }
-  };
+export const getLiveClassTeachersAsync = async ({ dispatch, callbackFn, data, token }) => {
+  try {
+    const URL = `${BASEURL}${GET_LIVE_CLASS_TEACHERS}`;
+    const result = getAPICall(URL, data, token).then((res) => {
+      callbackFn && callbackFn(res);
+      return res;
+    });
+    dispatch(getLiveClassTeachers({ isLoading: false, response: result.data }));
+  } catch (err) {
+    dispatch(getLiveClassTeachers({ isLoading: false }));
+  }
+};
 
 
 
 export const resetAsync = () => async (dispatch) => {
   dispatch(reset());
 };
-export const { getTeachers,getTeacherStats,updateTeacher,addTeacher,getTeacherDetails,getLocalGov,getLiveClassTeachers,deleteTeacher} =
+export const { getTeachers, getTeacherStats, updateTeacher, addTeacher, getTeacherDetails, getLocalGov, getLiveClassTeachers, deleteTeacher } =
   teacherSlice.actions;
 
 export const getTeachersResponse = (state) => state.teacher.getTeachersResponse;
